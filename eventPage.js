0e6a7c8c66e28d6c4c7ed80be5b8823f686b152b
Paul Ellenbogen <pe5@cs.princeton.edu>
2017-05-31 16:35:30 -0400

    Resolve conflicts

--- eventPage.js
+++ eventPage.js
@@ -2,71 +2,33 @@ var allPosts;
 var aggregateReturned;
 var start;
 var stop;
+var profile_name;
 
 chrome.runtime.onMessage.addListener(
   function(request, sender) {
     console.log("Message on the event page received.");
     console.log(request);
     console.log(sender);
-    if (sender.url.search("/timeline") < 0) { // This is from page
-      start = request.start;
-      stop = request.stop;
-      allPosts = [];
-      handleMainPosts(request);
-    } else {
-      handleAggregatedPosts(request);
-    }
+    start = request.start;
+    stop = request.stop;
+    profile_name = request.profile_name;
+    allPosts = [];
+    handleMainPosts(request);
+
   });
 
 function handleMainPosts(request) {
   let tmpPosts = deserializePosts(request.posts);
   addUniquePosts(tmpPosts);
-  let aggregateUrls = request.aggregate;
-  if (aggregateUrls.length === 0) {
-    download();
-    return;
-  }
-  initAggregateReturned(aggregateUrls.length);
-  let ids = [];
-  for (let i = 0; i < aggregateUrls.length; i++) {
-    chrome.tabs.create({ url:  aggregateUrls[i]}, function(tab) {
-      console.log("Opening tab");
-      console.log(tab);
-      chrome.tabs.executeScript(tab.id, {file: "util.js"});
-      chrome.tabs.executeScript(tab.id, {file: "aggregate_collect.js"});
-      ids.push(tab.id);
-    });
-  }
-  setTimeout(function() {
-  for (let i = 0; i < ids.length; i++) {
-    let id = ids[i];
-    chrome.tabs.sendMessage(id, {start: start, stop: stop, id: i});
-  }
-  }, 2000);
-}
-
-function initAggregateReturned(size) {
-  aggregateReturned = [];
-  for (let i = 0; i < size; i++) {
-    aggregateReturned.push(false);
-  }
-}
-
-function handleAggregatedPosts(request) {
-  console.log("Aggregate page " + request.id + " returned");
-  console.log(request);
-  let posts = deserializePosts(request.posts);
-  addUniquePosts(posts);
-  aggregateReturned[request.id] = true;
-  if (aggregateReturned.every(x => x)) {
-    download();
-  }
+  download();
 }
 
 function download() {
   let postStr = JSON.stringify(serializePosts(allPosts));
   let downloadStr = "data:text/json;charset=utf8," + encodeURIComponent(postStr);
-  chrome.downloads.download({url: downloadStr, filename: "fb.json"});
+  let date_str = new Date().toISOString().slice(0,10);
+  let filename = profile_name + "_" + date_str + ".json";
+  chrome.downloads.download({url: downloadStr, filename: filename});
 }
 
 function addUniquePosts(posts) {
